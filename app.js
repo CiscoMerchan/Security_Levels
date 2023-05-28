43374bc26882ee904273ebf0a11b42b8f1e1c0e6
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-28 14:18:51 +1000

    First commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const  express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
+// Initialize app with express
 const app = express();
 
 app.use(express.static('public'));
@@ -10,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+// ROUTE
 app.get('/', function(req, res){
     res.render('home.ejs');
 });
@@ -23,9 +24,7 @@ app.get('/register', function(req, res){
     res.render('register.ejs')
 });
 
-
-
-
+/*The web app will be display at : http://localhost:3030/ */ 
 app.listen(3030, function(){
     console.log('Server 3030')
 });
